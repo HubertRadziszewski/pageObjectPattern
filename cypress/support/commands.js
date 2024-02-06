c1c8e53306ec0efab4b873a728b4866f5a6bbdf7
HubertR <118179655+HubertRadziszewski@users.noreply.github.com>
2024-02-06 22:16:20 +0100

    Delete cypress directory

--- cypress/support/commands.js
+++
@@ -1,5 +0,0 @@
-Cypress.Commands.add('loginUser', (email, password) => {
-    cy.get('#user_email').type(email);
-    cy.get('[name="password"]').type(password);
-    cy.get('button[type="submit"]').click();
-});
