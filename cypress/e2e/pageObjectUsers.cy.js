7a07ec7838e7a0958a5938b7b627eb084927363b
HubertR <118179655+HubertRadziszewski@users.noreply.github.com>
2024-02-07 15:13:20 +0100

    Add files via upload

---
+++ cypress/e2e/pageObjectUsers.cy.js
@@ -0,0 +1,46 @@
+import LoginPage from "../pages/loginPage"
+import HomePage from "../pages/homePage"
+
+const loginPage = new LoginPage();
+const homePage = new HomePage();
+
+describe('Check Login', () => {
+    beforeEach('go to page', () => {
+        cy.visit('/');
+    });
+
+    it('User 1 successfully logs in and out from the GoIT page', () => {
+        cy.fixture('user1.json').then((user) => {
+            const useremail = user.email;
+            const password = user.password;
+
+            loginPage.loginUser(useremail, password);
+            cy.wait(5000);
+            cy.url().should('include', 'homepage');
+    
+                homePage.logout();
+                cy.wait(5000);
+
+    });
+    });
+});
+    describe('Check Login', () => {
+        beforeEach('go to page', () => {
+            cy.visit('/');
+        });
+    
+        it('User 2 successfully logs in and out from the GoIT page', () => {
+            cy.fixture('user2.json').then((user) => {
+                const useremail = user.email;
+                const password = user.password;
+    
+                loginPage.loginUser(useremail, password);
+                cy.wait(5000);
+                cy.url().should('include', 'homepage');
+        
+                    homePage.logout();
+                    cy.wait(5000);
+    
+        });
+        });
+});
